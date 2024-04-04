5b5e36535a72fd468136ba8bb658adce450656be
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-04-04 14:33:08 +0600

    .

--- apps/product/models.py
+++ apps/product/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за единицу')
     retail_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за мелкий опт')
     wholesale_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за крупный опт')
-
+    min_quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
--- apps/urls.py
+++ apps/urls.py
@@ -29,12 +29,12 @@ urlpatterns = [
     path('select_user_type/', SelectUserTypeView.as_view(), name='select_user_type'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ViewProfile.as_view(), name='view_profile'),
-]
+
     # products
-    path('products', ProductListView.as_view(), name='product_list'),
+    path('products/', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-]
+
 ]
