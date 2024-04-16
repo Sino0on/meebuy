b6253e5f062d8582d30abe106a828d2ff26ab7ec
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-04-16 15:21:07 +0600

    admin apnel customize

--- apps/product/models.py
+++ apps/product/models.py
@@ -3,16 +3,16 @@ from apps.provider.models import Category
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=123)
-    description = models.TextField(blank=True, null=True)
-    type = models.CharField(max_length=100, blank=True, null=True)
-    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    title = models.CharField(max_length=123, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    type = models.CharField(max_length=100, blank=True, null=True, verbose_name='Тип')
+    manufacturer = models.CharField(max_length=100, blank=True, null=True, verbose_name='Производитель')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за единицу')
     retail_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за мелкий опт', blank=True, null=True)
     wholesale_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за крупный опт', blank=True, null=True)
     min_quantity = models.PositiveIntegerField(default=1, verbose_name='Минимальное количество')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Времея обновления')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
     phone = models.CharField(max_length=123, blank=True, null=True, verbose_name='Телефон')
     terms_of_sale = models.CharField(max_length=200, blank=True, null=True, verbose_name='Условия продажи')
