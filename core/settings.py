32ab3197578e8bb53b76ebdd812aeda9f7966fee
tturdumamatovv <muki2022@mail.ru>
2024-05-26 00:08:55 +0600

    a

--- core/settings.py
+++ core/settings.py
@@ -282,11 +282,11 @@ MODELTRANSLATION_LANGUAGES = ('ru', 'ky')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "apps/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "apps/static")
 STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "apps/static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "apps/static"),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "apps/media")
 MEDIA_URL = "/media/"
