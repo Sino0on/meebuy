c0162554778600abd036ea4191f3dafc9a9c6be8
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-05-27 18:09:25 +0600

    little fixes

--- apps/product/views.py
+++ apps/product/views.py
@@ -68,7 +68,7 @@ class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'cabinet/products.html'
-    success_url = '/products'
+    success_url = reverse_lazy('user_products')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
