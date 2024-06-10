29ad61323d47e698ec30aa6641185730e5c758d6
Bekjan02 <bayganaliev@mail.ru>
2024-06-10 14:19:18 +0600

    added tabs

--- apps/static/pages/suppliers/suppliers.js
+++ apps/static/pages/suppliers/suppliers.js
@@ -1,51 +1,19 @@
-// dropdown mobile sort
-const dropdownBtn = document.querySelector(".dropdown__btn");
-const dropdownMenu = document.querySelector(".dropdown__menu");
-const dropdownItems = dropdownMenu.querySelectorAll(".dropdown__item");
 
-const openSort = () => {
-  const isActive = dropdownMenu.classList.contains("active");
-  dropdownMenu.style.maxHeight = isActive
-    ? "0"
-    : `${dropdownMenu.scrollHeight}px`;
-  dropdownMenu.classList.toggle("active");
-};
-
-dropdownBtn.addEventListener("click", openSort);
-
-document.addEventListener("DOMContentLoaded", function () {
-  dropdownItems.forEach(function (item) {
-    item.addEventListener("click", function (event) {
-      event.preventDefault();
-      const text = item.textContent;
-      changeButtonText(text);
-      openSort();
-    });
+// tabs
+function openTab(_, tabId) {
+  const contents = document.querySelectorAll('.tab-content');
+  contents.forEach(content => {
+      content.style.display = 'none';
   });
-});
 
-function changeButtonText(text) {
-  dropdownBtn.querySelector("p").textContent = text;
-}
+  const allTabs = document.querySelectorAll('.tabs__item');
+  allTabs.forEach(tab => {
+      tab.classList.remove('active');
+  });
 
-// sort dropdown desktop
-const all = document.getElementById("all");
-const suppliers = document.getElementById("suppliers");
-const manufactures = document.getElementById("manufactures");
-const logistics = document.getElementById("logistics");
-
-const toggleSort = (el) => {
-  const tab = [all, suppliers, manufactures, logistics];
-
-  for (let i = 0; i <= tab.length; i++) {
-    if (tab[i] === el) {
-      tab[i].style.backgroundColor = "#fffb98";
-    } else {
-      tab[i].style.backgroundColor = "";
-    }
+  document.getElementById(tabId).style.display = 'block';
+  const activeTab = Array.from(allTabs).find(tab => tab.onclick.toString().includes(`'${tabId}'`));
+  if (activeTab) {
+      activeTab.classList.add('active');
   }
-};
-all.addEventListener("click", () => toggleSort(all));
-suppliers.addEventListener("click", () => toggleSort(suppliers));
-manufactures.addEventListener("click", () => toggleSort(manufactures));
-logistics.addEventListener("click", () => toggleSort(logistics));
+}
