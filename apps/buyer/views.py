47111152b4fc4995b0a532952e7184d10abb730f
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-06-10 16:58:42 +0600

    buyer_functionality

--- apps/buyer/views.py
+++ apps/buyer/views.py
@@ -3,28 +3,30 @@ from django.views import generic
 
 from apps.authentication.models import User
 from apps.buyer.filters import BuyerFilter
-from apps.provider.models import Provider
+from apps.provider.filters import ProviderFilter
+from apps.provider.models import Provider, Category
 from apps.user_cabinet.models import Contacts
 
 
 class BuyerListView(generic.ListView):
     template_name = "buyer/buyers.html"
     context_object_name = "buyers"
-    model = User
+    model = Provider
     paginate_by = 20
 
     def get_queryset(self):
-        queryset = User.objects.filter(is_active=True, provider__title__isnull=False)
+        queryset = Provider.objects.filter(is_active=True, title__isnull=False, user__tenders__isnull=False)
         order = self.request.GET.get("order")
         if order:
             queryset = queryset.order_by(order)
-        self.filter = BuyerFilter(self.request.GET, queryset=queryset)
+        self.filter = ProviderFilter(self.request.GET, queryset=queryset)
         return self.filter.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         contacts = Contacts.load()
         context["contacts"] = contacts
+        context['categories'] = Category.objects.all()
         return context
 
 
