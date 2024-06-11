5f1c55583e4a58d79eb902c47872b2b2b8eecffc
tturdumamatovv <muki2022@mail.ru>
2024-06-11 17:12:01 +0600

    a

--- apps/user_cabinet/views.py
+++ apps/user_cabinet/views.py
@@ -763,7 +763,7 @@ def freedompay_success(request):
             pg_payment_id = request.GET.get('pg_payment_id')
             if not pg_payment_id:
                 user = User.objects.get(id=request.user.id)
-                pg_payment_id = Transaction.object.filter(user=user)[-1].pg_payment_id
+                pg_payment_id = Transaction.objects.filter(user=user)[-1].pg_payment_id
 
 
         except json.JSONDecodeError:
