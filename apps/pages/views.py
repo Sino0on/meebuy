ec7abee31417617c00bbf24dc2ef3abe298adcb6
tturdumamatovv <muki2022@mail.ru>
2024-06-13 14:39:53 +0600

    new changes

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 def privacy_policy_view(request):
     privacy_policy, created = StaticPage.objects.get_or_create(slug='provicy')
     if created:
-        privacy_policy.title = 'Политики конфиденциальности'
-        privacy_policy.description = 'Здесь вы можете создать описание "Политики конфиденциальности" для вашего сайта meebuy.ru'
+        privacy_policy.title = 'Политика конфиденциальности'
+        privacy_policy.description = 'Здесь вы можете создать описание "Политика конфиденциальности" для вашего сайта meebuy.ru'
         privacy_policy.save()
         return render(request, 'staticpages/privacy_policy.html', {'policy': privacy_policy})
     else:
@@ -16,8 +16,8 @@ def privacy_policy_view(request):
 def rules_view(request):
     rules, created = StaticPage.objects.get_or_create(slug='rules')
     if created:
-        rules.title = 'Политики конфиденциальности'
-        rules.description = 'Здесь вы можете создать описание "Политики конфиденциальности" для вашего сайта meebuy.ru'
+        rules.title = 'Правила'
+        rules.description = 'Здесь вы можете создать описание "Правила" для вашего сайта meebuy.ru'
         rules.save()
         return render(request, 'staticpages/rules.html', {'rules': rules})
     else:
