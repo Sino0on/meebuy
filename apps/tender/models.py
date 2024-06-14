abd1e2ef25af2415f6c23783e3c997a7386933f2
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-06-14 15:02:28 +0600

    code for countries

--- apps/tender/models.py
+++ apps/tender/models.py
@@ -12,6 +12,7 @@ User = get_user_model()
 
 class Country(models.Model):
     title = models.CharField(max_length=123)
+    code = models.CharField(max_length=5)
 
     def __str__(self):
         return f"{self.title}"
@@ -46,38 +47,38 @@ class City(models.Model):
 
 
 class Tender(StatusMixin, models.Model):
-    title = models.CharField(max_length=123)
-    description = models.TextField(blank=True, null=True)
-    price = models.PositiveIntegerField(blank=True, default=0, null=True)
+    title = models.CharField(max_length=123, verbose_name="Название")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    price = models.PositiveIntegerField(blank=True, default=0, null=True, verbose_name="Цена")
     currency = models.CharField(
-        max_length=100, verbose_name=_("Валюта"), blank=True, null=True
+        max_length=100, verbose_name="Валюта", blank=True, null=True
     )
-    quantity = models.PositiveIntegerField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(blank=True, null=True, verbose_name="Количество")
     category = models.ForeignKey(
-        Category, on_delete=models.PROTECT, blank=True, null=True
+        Category, on_delete=models.PROTECT, blank=True, null=True, verbose_name="Категория"
     )
-    city = models.ForeignKey(City, on_delete=models.PROTECT, blank=True, null=True)
-    phone = models.CharField(max_length=123, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.PROTECT, blank=True, null=True, verbose_name="Город")
+    phone = models.CharField(max_length=123, blank=True, null=True, verbose_name="Телефон")
+    email = models.EmailField(blank=True, null=True, verbose_name="Электронная почта")
     user = models.ForeignKey(
-        User, related_name="tenders", on_delete=models.SET_NULL, null=True, blank=True
+        User, related_name="tenders", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Пользователь"
     )
-    requirements = models.CharField(max_length=123, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    end_date = models.DateField(blank=True, null=True)
-    place_of_sale = models.CharField(max_length=123, blank=True, null=True)
+    requirements = models.CharField(max_length=123, blank=True, null=True, verbose_name="Требования")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    end_date = models.DateField(blank=True, null=True, verbose_name="Дата окончания")
+    place_of_sale = models.CharField(max_length=123, blank=True, null=True, verbose_name="Место продажи")
 
-    is_phone = models.BooleanField(default=False, blank=True, null=True)
-    is_active = models.BooleanField(default=True, blank=True, null=True)
+    is_phone = models.BooleanField(default=False, blank=True, null=True, verbose_name="Наличие телефона")
+    is_active = models.BooleanField(default=True, blank=True, null=True, verbose_name="Активен")
     # Payment Types
-    cash = models.BooleanField(default=False, verbose_name=_("Наличными"))
+    cash = models.BooleanField(default=False, verbose_name="Наличными")
     bank_transfer = models.BooleanField(
-        default=False, verbose_name=_("Безналичная оплата")
+        default=False, verbose_name="Безналичная оплата"
     )
-    credit_card = models.BooleanField(default=False, verbose_name=_("Кредитные карты"))
+    credit_card = models.BooleanField(default=False, verbose_name="Кредитные карты")
     electronic_money = models.BooleanField(
-        default=False, verbose_name=_("Электронные деньги")
+        default=False, verbose_name="Электронные деньги"
     )
 
     def __str__(self):
