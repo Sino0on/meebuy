77d1ebde717fd8d219cb26ad0cd0573e2e6c5774
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-09-04 17:45:57 +0600

    4.09

--- apps/provider/signals.py
+++ apps/provider/signals.py
@@ -1,15 +1,26 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from .models import Provider
 
+@receiver(pre_save, sender=Provider)
+def capture_previous_state(sender, instance, **kwargs):
+    # Проверяем, существует ли уже запись в базе данных
+    if instance.pk:
+        # Загружаем предыдущее состояние из базы данных
+        previous = sender.objects.get(pk=instance.pk)
+        instance._previous_moderation_state = previous.is_modered
+    else:
+        instance._previous_moderation_state = None
+
 
 @receiver(post_save, sender=Provider)
 def provider_moderation_signal(sender, instance, created, **kwargs):
-    if not created and instance.is_modered:
+    # Проверяем, что запись не новая и что состояние изменилось с False на True
+    if not created and getattr(instance, '_previous_moderation_state', None) == False and instance.is_modered:
         user_email = instance.user.email
         instance.comment = 'Вы успешно прошли модерацию, ваш аккаунт полностью активен.'
         subject = 'Поставщик прошел модерацию'
